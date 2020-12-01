64f1eae9b9677dfc2eb26d6133a15f48848e7f5c
Quentin PLAUD <qplaud@gmail.com>
2020-12-01 10:55:06 +0100

    Branchement Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  
+  # Test implémentation devise
+  root 'static_pages#index'
+  get 'static_pages/secret'
+  
+  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
