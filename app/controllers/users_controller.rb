7d1602355c40412f5b79ed4618eca2717e7fd2b2
berangereh <hermann.berangere@gmail.com>
2020-12-01 20:45:37 +0100

    page profil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,12 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :user_exists?, :check_user, only: [:show]
 
+
+
   def show
     @user = current_user
+
+   
   end
 
   def edit
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(email: 'pulpoludo@yopmail.com', first_name: 'pulpo', last_name: 'ludo', password: 'mdp123')
