d5adc756ef5b7b6b53b455345379b6859fe9eff2
Victor-Dauth <dautheville.victor@gmail.com>
2020-12-02 23:17:39 +0100

    fix issue with carts migration

--- db/migrate/20201202135853_create_carts.rb
+++ db/migrate/20201202135853_create_carts.rb
@@ -1,7 +1,7 @@
 class CreateCarts < ActiveRecord::Migration[5.2]
   def change
     create_table :carts do |t|
-      t.string :statuses     null:false, default:"panier actuel"
+      t.string :statuses,     null:false, default:"panier actuel"
 
       t.belongs_to :user,    index: true
 
