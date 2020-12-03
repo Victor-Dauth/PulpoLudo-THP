30f5e67e2c542944cf041c7ad59faf282e7cfba3
Quentin PLAUD <qplaud@gmail.com>
2020-12-03 17:14:52 +0100

    Implementation addresses and sync with users

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,21 @@
 Rails.application.routes.draw do
   
   root to: 'game_sheets#index'
-  get 'static_pages/index'
   
-
-
-  devise_for :users
-
-
-
-  resources :users do
-    resources :avatars, only: [:create]
-  end
-
-  resources :carts, only: [:show, :update]
-
   resources :game_sheets, only: [:index, :show] do
     resources :game_pictures, only: [:create]
   end
 
-  resources :subscriptions
+  devise_for :users
 
-  
+  resources :users do
+    resources :avatars, only: [:create]
+    resources :addresses, except: [:show]
+  end
+
+  resources :carts, only: [:show, :update]
+
+  resources :subscriptions
 
   scope '/checkout' do
     post 'create', to: 'checkout#create', as: 'checkout_create'
