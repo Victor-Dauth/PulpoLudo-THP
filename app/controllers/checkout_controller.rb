24a975c62b89e16450e5b50c596802b2501101d2
Quentin PLAUD <qplaud@gmail.com>
2020-12-08 12:48:14 +0100

    Migration Stripe towards sub model ok

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,41 +3,28 @@ class CheckoutController < ApplicationController
   before_action :authenticate_user!
   
   def create
-    @user = current_user
-    @price = 10
-
     @session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
-      line_items: [
-        {
-          name: 'Rails Stripe Checkout',
-          amount: (@price*100).to_i,
-          currency: 'eur',
-          quantity: 1
-        },
-      ],
+        mode: 'subscription',
+        line_items: [{
+          # For metered billing, do not pass quantity
+          quantity: 1,
+          price: 'price_1Hw2XoL1Cw86LE2tOtVVb7TL',
+        }],
       success_url: checkout_success_url + '?session_id={CHECKOUT_SESSION_ID}',
       cancel_url: checkout_cancel_url
     )
-
     respond_to do |format|
       format.js # renders create.js.erb
     end
-
   end
 
   def success
-    @session = Stripe::Checkout::Session.retrieve(params[:session_id])
-    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
-    
     @paid_subscription = build_subscription
     new_subscription_email(@paid_subscription)
   end
 
   def cancel
-    @session = Stripe::Checkout::Session.retrieve(params[:session_id])
-    @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
-
     @not_paid_subscription = build_subscription_error
     failure_subscription_email(@not_paid_subscription)
   end
@@ -45,21 +32,19 @@ class CheckoutController < ApplicationController
   private
   
   def build_subscription
-    Subscription.create(user: current_user, status: 'actif', price: 10, start_date: Time.now, duration: 1)
+    Subscription.create(user: current_user, status: 'actif', price: 9.99, start_date: Time.now, duration: 1)
   end
 
   def build_subscription_error
-    Subscription.create(user: current_user, status: "en attente", price: 10, start_date: Time.now, duration: 1)
+    Subscription.create(user: current_user, status: "en attente", price: 9.99, start_date: Time.now, duration: 1)
   end
 
   def failure_subscription_email(subscription)
-
     UserMailer.issue_subscription_email(subscription).deliver_now
     AdminMailer.issue_subscription_email_admin(subscription).deliver_now
   end
 
   def new_subscription_email(subscription)
-
     UserMailer.new_subscription_email(subscription).deliver_now
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,8 @@ Rails.application.routes.draw do
 
   scope '/checkout' do
     post 'create', to: 'checkout#create', as: 'checkout_create'
-    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
     get 'success', to: 'checkout#success', as: 'checkout_success'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
   end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
