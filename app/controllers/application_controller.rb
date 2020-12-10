0d5a533735acd8dc862e17fc6dbba1af6b0a1992
Victor-Dauth <dautheville.victor@gmail.com>
2020-12-10 14:07:42 +0100

    redirection for sub adress and profil

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,16 @@ class ApplicationController < ActionController::Base
   include CheckUser
 
   #Start Method to modify Devise 
-  def after_sign_in_path_for(resource)
+  def after_sign_up_path_for(resource)
     user_subscriptions_path(current_user)
+  end
+
+  def after_sign_in_path_for(resource)
+    if current_user.already_subscribed?
+      root_path
+    else
+      user_subscriptions_path(current_user)
+    end
   end
 
   #End Method to modify Devise 
