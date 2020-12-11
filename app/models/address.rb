d763923edba41871e39116a0fe29dea171429396
Victor-Dauth <dautheville.victor@gmail.com>
2020-12-11 17:53:16 +0100

    add validation +title in back for adress

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,18 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  validates :title,
+    presence: true
+
+  validates :street,
+    presence: true
+
+  validates :zipcode,
+    presence: true
+
+  validates :city,
+    presence: true,
+
+  validates :country,
+    presence: true,
 end
