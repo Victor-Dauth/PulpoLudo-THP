45c4c8dfa6d5348aa77ec91d6ba143762ef2d08b
VeraTuran <vmeturan@gmail.com>
2020-12-11 19:37:03 +0100

    doubtful validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,20 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  validates :street, :city, presence: true
+  	presence: true
+
+  validates :zipcode
+  	presence: true
+  	numericality: {greater_than_or_equal_to: 0}
+
+  validates :city, 
+  	presence: true
+  	format: { with: /\A[a-zA-Z]+\z/, message: "ne peut contenir que des lettres" }
+
+  validates :country, 
+  	presence: true
+  	format: { with: /\A[a-zA-Z]+\z/, message: "ne peut contenir que des lettres" }
+
+
 end
