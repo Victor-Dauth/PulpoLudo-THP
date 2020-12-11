71ebd61216b2ac55b6917520d301e669e00d4929
Victor-Dauth <dautheville.victor@gmail.com>
2020-12-11 20:16:38 +0100

    not game_sheet in index if no game in_stock

--- app/controllers/game_sheets_controller.rb
+++ app/controllers/game_sheets_controller.rb
@@ -3,9 +3,7 @@ class GameSheetsController < ApplicationController
   before_action :authenticate_user!, only: [:show]
   
   def index
-  	
-    @game_sheets = GameSheet.all
-
+    @game_sheets = all_game_sheets_in_stock
   end
 
   def show
@@ -13,5 +11,16 @@ class GameSheetsController < ApplicationController
     @game_sheets_rand = GameSheet.all.reject {|game_sheet| game_sheet == @game_sheet }.sample(3)
   end
 
+  private 
 
+  def all_game_sheets_in_stock
+    games = Game.in_stock
+    game_sheets = []
+
+    games.each do |game|
+      game_sheets.push(game.game_sheet) unless game_sheets.include?(game.game_sheet)
+    end
+
+    return game_sheets
+  end
 end
